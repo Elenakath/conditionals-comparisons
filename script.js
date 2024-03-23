5fa0ca2038067f764c180c589889fe13aacddee5
elena_e <elenaka123@gmail.com>
2024-03-23 09:41:33 -0500

    Inital commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var string = "12";
+var numerical = 12;
+if (string === numerical) {
+    console.log("The values and types are equal");
+} else if ( string == numerical) {
+    console.log("The values are equal");
+} else {
+    console.log("Neither condition is met");
+}
